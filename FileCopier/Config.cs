815b893ab3ec62c6557c9a46c91f217b20729edc
Anton Konet <konet87@gmail.com>
2026-03-25 11:33:36 +0100

    Initial commit

---
+++ FileCopier/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCopier
+{
+    internal class Config
+    {
+        public string SourceFolder { get; set; }
+        public string TargetFolder { get; set; }
+        public string SourceFileName { get; set; }
+        public string Template { get; set; }
+        public string Prefix { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ FileCopier/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text.Json;
+
+namespace FileCopier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string configFilePath = Path.Combine(AppContext.BaseDirectory, "config.json");
+            string json = File.ReadAllText(configFilePath);
+
+            Config config = JsonSerializer.Deserialize<Config>(json);
+
+            string sourceFilePath = Path.Combine(config.SourceFolder, config.SourceFileName);
+            string targetFileName = BuildTargetFileName(config.Template, config.Prefix, config.Id, config.TargetFolder);
+            string targetFilePath = Path.Combine(config.TargetFolder, targetFileName);
+            File.Copy(sourceFilePath, targetFilePath);
+        }
+
+        private static string BuildTargetFileName(string template, string prefix, string id, string targetFolder)
+        {
+            string fileName = template;
+
+            fileName = fileName.Replace("%prefix%", prefix);
+
+            int idLength = id.Length;
+            int freeId = int.Parse(id);
+
+            string[] filesNames = Directory.GetFiles(targetFolder);
+            int[] takenIds = filesNames.Select(
+                s => int.Parse(Path.GetFileName(s).Substring(prefix.Length + 1, idLength))
+            ).ToArray();
+
+            while (takenIds.Contains(freeId) && freeId < 9999)
+                freeId += 1;
+
+            fileName = fileName.Replace("%id%", freeId.ToString($"D{idLength}"));
+
+            fileName = fileName.Replace("%timestamp%", DateTime.Now.ToString("yyyyMMddHHmmss"));
+
+            return fileName;
+        }
+    }
+}
